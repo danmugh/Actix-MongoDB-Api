be115991dc065cef47298988ac9aceb5947930f7
Dan Mugisho <danmugisho@Dans-MacBook-Pro.local>
2022-07-09 01:25:38 +0300

    Initiate project

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+mod api;
+mod models;
+mod repository;
+
+use actix_web::{web::Data, App, HttpServer};
+use api::user_api::{create_user, get_user, update_user, delete_user, get_all_users};
+use repository::mongodb_repo::MongoRepo;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_user)
+            .service(get_user)
+            .service(update_user)
+            .service(delete_user)
+            .service(get_all_users)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
---
+++ src/repository/mongodb_repo.rs
@@ -0,0 +1,111 @@
+use std::env;
+extern crate dotenv;
+use dotenv::dotenv;
+
+use mongodb::{
+    bson::{extjson::de::Error, oid::ObjectId, doc},
+    results::{ InsertOneResult, UpdateResult, DeleteResult},
+    Client, Collection,
+};
+
+use crate::models::user_model::User;
+use futures::stream::TryStreamExt;
+
+pub struct MongoRepo {
+    col: Collection<User>,
+}
+
+impl MongoRepo {
+    pub async fn init() -> Self {
+        dotenv().ok();
+        let uri = match env::var("MONGOURI") {
+            Ok(v) => v.to_string(),
+            Err(_) => format!("Error loading env variable"),
+        };
+        let client = Client::with_uri_str(uri).await.expect("Error connecting to database");
+        let db = client.database("rustDB");
+        let col: Collection<User> = db.collection("User");
+        MongoRepo { col }
+    }
+
+    pub async fn create_user(&self, new_user: User) -> Result<InsertOneResult, Error> {
+        let new_doc = User {
+            id: None,
+            name: new_user.name,
+            location: new_user.location,
+            title: new_user.title,
+        };
+        let user = self
+            .col
+            .insert_one(new_doc, None)
+            .await
+            .ok()
+            .expect("Error creating user");
+        Ok(user)
+    }
+
+    pub async fn get_user(&self, id: &String) -> Result<User, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let user_detail = self
+            .col
+            .find_one(filter, None)
+            .await
+            .ok()
+            .expect("Error getting user's detail");
+        Ok(user_detail.unwrap())
+    }
+
+    pub async fn update_user(&self, id: &String, new_user: User) -> Result<UpdateResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let new_doc = doc! {
+                "$set":
+                    {
+                        "id": new_user.id,
+                        "name": new_user.name,
+                        "location": new_user.location,
+                        "title": new_user.title
+                    },
+            };
+        let updated_doc = self
+            .col
+            .update_one(filter, new_doc, None)
+            .await
+            .ok()
+            .expect("Error updating user");
+        Ok(updated_doc)
+    }
+
+    pub async fn delete_user(&self, id: &String) -> Result<DeleteResult, Error> {
+        let obj_id = ObjectId::parse_str(id).unwrap();
+        let filter = doc! {"_id": obj_id};
+        let user_detail = self
+            .col
+            .delete_one(filter, None)
+            .await
+            .ok()
+            .expect("Error deleting user");
+        Ok(user_detail)
+    }
+
+    pub async fn get_all_users(&self) -> Result<Vec<User>, Error> {
+        let mut cursors = self
+            .col
+            .find(None, None)
+            .await
+            .ok()
+            .expect("Error getting list of users");
+        let mut users: Vec<User> = Vec::new();
+        while let Some(user) = cursors
+            .try_next()
+            .await
+            .ok()
+            .expect("Error mapping through cursor")
+        {
+            users.push(user)
+        }
+        Ok(users)
+    }
+}
+
